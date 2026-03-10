e5ce62f20cb97f2af0926c705120bff1b65c7b10
MAHESH18BM028 <maheshwari252000@gmail.com>
2026-03-10 18:27:55 +0530

    added pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    loadComponent: () => import('../pages/about/about-component').then(m => m.AboutComponent)
+  },
+  {
+    path: 'skills',
+    loadComponent: () => import('../pages/skills/skills-component').then(m => m.SkillsComponent)
+  },
+  
+];
