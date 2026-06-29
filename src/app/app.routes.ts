74d1d98e8eb3ebd6aff3f40643d33fb4bfc6f475
MAHESH18BM028 <maheshwari252000@gmail.com>
2026-06-29 15:57:51 +0530

    route  changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,8 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'about',
+    path: '',
     loadComponent: () => import('../pages/about/about-component').then(m => m.AboutComponent)
-  },
-  {
-    path: 'skills',
-    loadComponent: () => import('../pages/skills/skills-component').then(m => m.SkillsComponent)
-  },
+  }
   
 ];
